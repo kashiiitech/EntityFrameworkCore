c17e9aa1b8b021b2957b649ba89722f35ff5da9e
kashiiitech <kashiiitech@gmail.com>
2025-02-07 17:02:37 +0500

    Implementing data manipulation operations

--- EFCore.API/Controllers/MoviesController.cs
+++ EFCore.API/Controllers/MoviesController.cs
@@ -27,14 +27,28 @@ public class MoviesController : Controller
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get([FromRoute] int id)
     {
-        throw new NotImplementedException();
+        // Querris database, returns first match, null if not found
+        // var movie = await _context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+        // Similar to FirstOrDefault, but throws if more than one match is found.
+        // var movie = await _context.Movies.SingleOrDefaultAsync(m => m.Id == id);
+        
+        // Serves match from memory if already fetched, otherwise queries DB.
+        var movie = await _context.Movies.FindAsync(id);
+        
+        return movie != null ? Ok(movie) : NotFound();
     }
     
     [HttpPost]
     [ProducesResponseType(typeof(Movie), StatusCodes.Status201Created)]
     public async Task<IActionResult> Create([FromBody] Movie movie)
     {
-        throw new NotImplementedException();
+        await _context.Movies.AddAsync(movie);
+        
+        // movie has no ID
+        await _context.SaveChangesAsync();
+        // movie has an ID
+        
+        return CreatedAtAction(nameof(Get), new { id = movie.Id }, movie);
     }
     
     [HttpPut("{id:int}")]
@@ -42,7 +56,19 @@ public class MoviesController : Controller
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Update([FromRoute] int id, [FromBody] Movie movie)
     {
-        throw new NotImplementedException();
+        var existingMovie = await _context.Movies.FindAsync(id);
+
+        if (existingMovie == null)
+        {
+            return NotFound();
+        }
+        // we have movie we are going to update it
+        existingMovie.Title = movie.Title;
+        existingMovie.ReleaseDate = movie.ReleaseDate;
+        existingMovie.Synopsis = movie.Synopsis;
+        await _context.SaveChangesAsync();
+        
+        return Ok(existingMovie);
     }
     
     [HttpDelete("{id:int}")]
@@ -50,6 +76,16 @@ public class MoviesController : Controller
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Remove([FromRoute] int id)
     {
-        throw new NotImplementedException();
+        var existingMovie = await _context.Movies.FindAsync(id);
+
+        if (existingMovie == null)
+        {
+            return NotFound();
+        }
+
+        _context.Movies.Remove(existingMovie);
+        await _context.SaveChangesAsync();
+        
+        return Ok();
     }
 }
